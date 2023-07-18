d633ae8110d0e81e1bf0f815efff086a8e9c76b3
Shlomi Fish <shlomif@shlomifish.org>
2023-07-18 18:43:59 +0300

    shlomif-flag-demo

--- src/main.c
+++ src/main.c
@@ -391,6 +391,28 @@ bool init_grid(Grid* grid)
     return true;
 }
 
+                static void putpixel(
+                    SDL_Renderer* renderer,
+                    int x,
+                    int y,
+                    SDL_Color color
+                    )
+{
+    // Set renderer color to cell color
+    SDL_SetRenderDrawColor(renderer,
+                           color.r,
+                           color.g,
+                           color.b,
+                           color.a);
+
+                    SDL_Rect rect;
+                    rect.x=x;
+                    rect.y=y;
+                    rect.w=1;
+                    rect.h=1;
+                        // Render filled cell
+    SDL_RenderFillRect(renderer, &(rect));
+}
 
 void render_cell(Cell* cell, SDL_Renderer* renderer)
 {
@@ -1721,8 +1743,152 @@ bool start(SDL_Renderer* renderer, int width, int height)
 // FREE CODING SECTION
 //***************************************************************************************
 
+#define LOG2_DEGREES_RANGE 9
+#define DEGREES_RANGE (1 << (LOG2_DEGREES_RANGE))
+#define DEG_MOD(x) ((x)&(DEGREES_RANGE-1))
+
+static bool mycont = true;
 void draw(int x_cells, int y_cells)
 {
+    int x_num_points = 20;
+    int y_num_points = 10;
+    int x_x_offset = 20;
+    int x_y_offset = 0;
+    int y_x_offset = 3;
+    int y_y_offset = 10;
+    double y_amplitude = 10.0;
+    int y_amp_delta = 50;
+    int x_amp_delta = 20;
+    /* This is so it won't get out of the end of the screen */
+    int y_abs_offset = 20;
+    int x_abs_offset = 10;
+    int sin_lookup[DEGREES_RANGE];
+
+    int x,y,t, prev_t;
+    int prev_y_height;
+    int this_y_height;
+    int x_pos, y_pos;
+
+    int y_acc_degree, y_x_acc_degree;
+    int prev_y_acc_degree, prev_y_x_acc_degree;
+    int y_acc_y_pos, y_acc_x_pos;
+    SDL_Rect * rects;
+    SDL_Rect * the_rect;
+#define PI 3.14159265358
+    int a;
+
+    for(a=0 ; a < DEGREES_RANGE  ;a++)
+    {
+        sin_lookup[a] = (int)(sin((2*PI/DEGREES_RANGE)*a)*y_amplitude);
+    }
+
+    rects = (SDL_Rect *)malloc(sizeof(rects[0]) * x_num_points*y_num_points);
+    for(a=0;a<x_num_points*y_num_points;a++)
+    {
+        rects[a].w = 1;
+        rects[a].h = 2;
+    }
+
+#if 0
+    printf("%i\n", SDL_MUSTLOCK(screen));
+#endif
+
+    /*
+     * Calculate all the x positions of the rects in advance
+     * */
+    the_rect = rects;
+    y_acc_x_pos = x_abs_offset;
+    for(y=0;y<y_num_points;y++)
+    {
+        x_pos = y_acc_x_pos;
+        for(x=0;x<x_num_points;x++)
+        {
+            the_rect->x = x_pos;
+            the_rect++;
+            x_pos += x_x_offset;
+        }
+        y_acc_x_pos += y_x_offset;
+    }
+
+#if 0
+    {
+        SDL_Rect whole_screen_rect;
+        whole_screen_rect.x = whole_screen_rect.y = 0;
+        whole_screen_rect.w = 640;
+        whole_screen_rect.h = 480;
+        SDL_FillRect(screen,&whole_screen_rect, fill_color);
+        /* The 0's indicate that the whole screen needs to be updated */
+        SDL_UpdateRect(screen,0,0,0,0);
+    }
+#endif
+    prev_t = 0;
+    for(t=1 ; mycont ; t = ((t+1)&(DEGREES_RANGE-1)))
+    {
+        the_rect = rects;
+
+        y_acc_degree = t;
+        prev_y_acc_degree = prev_t;
+        y_acc_y_pos = y_abs_offset;
+        y_acc_x_pos = x_abs_offset;
+        for(y=0;y<y_num_points;y++)
+        {
+            y_x_acc_degree = y_acc_degree;
+            prev_y_x_acc_degree = prev_y_acc_degree;
+
+            x_pos = y_acc_x_pos;
+            y_pos = y_acc_y_pos;
+            for(x=0;x<x_num_points;x++)
+            {
+                prev_y_height =
+                    y_pos +
+                    sin_lookup[prev_y_x_acc_degree]
+                    ;
+                this_y_height =
+                    y_pos +
+                    sin_lookup[y_x_acc_degree]
+                    ;
+
+                the_rect->y = min(prev_y_height,this_y_height);
+                the_rect++;
+
+                /* I don't need to lock the screen, so I'm omitting it */
+
+#define screen g_renderer
+#define fill_color COLOR_WHITE
+#define pen_color  COLOR_BLACK
+
+                putpixel(
+                    screen,
+                    x_pos,
+                    prev_y_height,
+                    fill_color
+                );
+
+                putpixel(
+                    screen,
+                    x_pos,
+                    this_y_height,
+                    pen_color
+                );
+#undef screen
+
+
+                y_x_acc_degree = DEG_MOD(y_x_acc_degree+x_amp_delta);
+                prev_y_x_acc_degree = DEG_MOD(prev_y_x_acc_degree+x_amp_delta);
+                x_pos += x_x_offset;
+                y_pos += x_y_offset;
+            }
+            y_acc_degree = DEG_MOD(y_acc_degree+y_amp_delta);
+            prev_y_acc_degree = DEG_MOD(prev_y_acc_degree+y_amp_delta);
+            y_acc_y_pos += y_y_offset;
+            y_acc_x_pos += y_x_offset;
+        }
+    delay(500);
+    }
+
+#if 0
+        SDL_UpdateRects(screen, x_num_points*y_num_points, rects);
+#endif
     delay(500);
 
     set_cell_color(2, 1, COLOR_RED);
