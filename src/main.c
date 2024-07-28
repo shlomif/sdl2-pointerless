c0acee6a5a08e8615a657b0087e97738f4df75a7
Shlomi Fish <shlomif@shlomifish.org>
2024-07-28 15:13:40 +0300

    remove unused variables

--- src/main.c
+++ src/main.c
@@ -126,16 +126,6 @@
 
 const SDL_Color COLOR_BLACK         = _COLOR(0, 0, 0);
 const SDL_Color COLOR_WHITE         = _COLOR(0xFF, 0xFF, 0xFF);
-const SDL_Color COLOR_GRAY          = _COLOR(0x64, 0x64, 0x64);
-const SDL_Color COLOR_DARK_GRAY     = _COLOR(0x1E, 0x1E, 0x1E);
-const SDL_Color COLOR_LIGHT_GRAY    = _COLOR(0xC8, 0xC8, 0xC8);
-const SDL_Color COLOR_RED           = _COLOR(0xF5, 0x3B, 0x56);
-const SDL_Color COLOR_GREEN         = _COLOR(0x01, 0x9F, 0x13);
-const SDL_Color COLOR_BLUE          = _COLOR(0x38, 0x95, 0xD3);
-const SDL_Color COLOR_YELLOW        = _COLOR(0xF7, 0xDC, 0x11);
-const SDL_Color COLOR_ORANGE        = _COLOR(0xFF, 0x85, 0);
-const SDL_Color COLOR_PINK          = _COLOR(0xFF, 0, 0xCE);
-const SDL_Color COLOR_VIOLET        = _COLOR(0x91, 0, 0xFF);
 const SDL_Color NO_COLOR            = COLOR_WHITE;
 
 enum
@@ -823,16 +813,6 @@ bool start(SDL_Renderer* renderer, int width, int height)
 //
 //      COLOR_BLACK
 //      COLOR_WHITE
-//      COLOR_GRAY
-//      COLOR_DARK_GRAY
-//      COLOR_LIGHT_GRAY
-//      COLOR_RED
-//      COLOR_GREEN
-//      COLOR_BLUE
-//      COLOR_YELLOW
-//      COLOR_ORANGE
-//      COLOR_PINK
-//      COLOR_VIOLET
 //
 // Custom color:
 //
